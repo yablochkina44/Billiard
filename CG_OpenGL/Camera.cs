3bbf3c43f9ece1cbec9248f10cefa1686ec37379
yablochkina44 <73835957+yablochkina44@users.noreply.github.com>
2022-05-13 21:45:29 +0300

    Add files via upload

---
+++ CG_OpenGL/Camera.cs
@@ -0,0 +1,118 @@
+﻿using OpenTK.Mathematics;
+using System;
+
+namespace CommonClasses
+{
+    // Это класс камеры.
+    // Важно отметить, что есть несколько способов, которыми вы могли бы настроить эту камеру.
+    // Например, вы могли бы также управлять вводом проигрывателя внутри класса camera,
+    // и многие свойства можно было бы превратить в функции.
+
+    // Если что-то в дальнейшем будет непонятно - смотри веб-версию. https://opentk.net/learn/chapter1/9-camera.html
+    public class Camera
+    {
+        // Эти векторы представляют собой направления, направленные наружу от камеры, чтобы определить, как она расположена.
+        private Vector3 _front = -Vector3.UnitZ;
+
+        private Vector3 _up = Vector3.UnitY;
+
+        private Vector3 _right = Vector3.UnitX;
+
+        // Поворот вокруг оси X (в радианах)
+        private float _pitch;
+
+        // Поворот вокруг оси Y (в радианах)
+        private float _yaw = -MathHelper.PiOver2; // Без этого вы начнёте повёрнутыми вправо на 90 градусов. Можете закомментить инициализацию и проверить
+
+        // Поле видимости камеры (в радианах).
+        private float _fov = MathHelper.PiOver2;
+
+        public Camera(Vector3 position, float aspectRatio)
+        {
+            Position = position;
+            AspectRatio = aspectRatio;
+        }
+
+        // Положение камеры
+        public Vector3 Position { get; set; }
+
+        // Это просто соотношение сторон видового экрана, используемое для матрицы проекции.
+        public float AspectRatio { private get; set; }
+
+        public Vector3 Front => _front;
+
+        public Vector3 Up => _up;
+
+        public Vector3 Right => _right;
+
+        // Мы преобразуем градусы в радианы, как только свойство настроено, для повышения производительности.
+        public float Pitch
+        {
+            get => MathHelper.RadiansToDegrees(_pitch);
+            set
+            {
+                // Мы фиксируем значение pitch между -89 и 89, чтобы предотвратить переворачивание камеры вверх ногами, и кучу
+                // странных ошибок, когда вы используете углы Эйлера для поворота.
+                // Если вы хотите узнать больше об этом, вы можете попробовать изучить тему под названием gimbal lock.
+                var angle = MathHelper.Clamp(value, -89f, 89f);
+                _pitch = MathHelper.DegreesToRadians(angle);
+                UpdateVectors();
+            }
+        }
+
+        // Мы преобразуем градусы в радианы, как только свойство настроено, для повышения производительности.
+        public float Yaw
+        {
+            get => MathHelper.RadiansToDegrees(_yaw);
+            set
+            {
+                _yaw = MathHelper.DegreesToRadians(value);
+                UpdateVectors();
+            }
+        }
+
+        // Поле зрения (FOV) - это вертикальный угол обзора камеры.
+        // Это более подробно обсуждалось в предыдущем уроке, но в этом уроке вы также узнали, как мы можем использовать это для имитации функции
+        // масштабирования.
+        // Мы преобразуем градусы в радианы, как только свойство настроено, для повышения производительности.
+        public float Fov
+        {
+            get => MathHelper.RadiansToDegrees(_fov);
+            set
+            {
+                var angle = MathHelper.Clamp(value, 1f, 90f);
+                _fov = MathHelper.DegreesToRadians(angle);
+            }
+        }
+
+        // Get the view matrix using the amazing LookAt function described more in depth on the web tutorials
+        public Matrix4 GetViewMatrix()
+        {
+            return Matrix4.LookAt(Position, Position + _front, _up);
+        }
+
+        // Получите матрицу проекции, используя тот же метод, который мы использовали до этого момента.
+        public Matrix4 GetProjectionMatrix()
+        {
+            return Matrix4.CreatePerspectiveFieldOfView(_fov, AspectRatio, 0.01f, 100f);
+        }
+
+        // Эта функция будет обновлять вершины направления, используя некоторые математические методы, изученные в веб-руководствах.
+        private void UpdateVectors()
+        {
+            // Во-первых, передняя матрица вычисляется с использованием некоторой базовой тригонометрии.
+            _front.X = MathF.Cos(_pitch) * MathF.Cos(_yaw);
+            _front.Y = MathF.Sin(_pitch);
+            _front.Z = MathF.Cos(_pitch) * MathF.Sin(_yaw);
+
+            // Нам нужно убедиться, что все векторы нормализованы, так как в противном случае мы получили бы некоторые странные результаты.
+            _front = Vector3.Normalize(_front);
+
+            // Вычислите как правый, так и указывающий вверх вектор, используя перекрестное произведение.
+            // Обратите внимание, что мы вычисляем справа от глобального направления "вверх"; такое поведение делает
+            // не то, что вам нужно для всех камер, так что имейте это в виду, если вам не нужна камера с частотой кадров в секунду.
+            _right = Vector3.Normalize(Vector3.Cross(_front, Vector3.UnitY));
+            _up = Vector3.Normalize(Vector3.Cross(_right, _front));
+        }
+    }
+}
---
+++ CG_OpenGL/Game.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+namespace CG_OpenGL
+{
+    public class Game : GameWindow
+    {
+        Vector3 LightPos = new Vector3(0.0f, 2.6f, 0.0f);
+        Shader shader;
+        
+
+        Texture DiffuseBoll1, SpecularBoll1;
+        Texture DiffuseBoll2, SpecularBoll2;
+        Texture DiffuseBoll3, SpecularBoll3;
+
+        List<ObjectRender> ObjectRenderList = new List<ObjectRender>();
+
+
+        double Time;
+        int Side = 1;
+        const double Degrees = 40;
+
+        //float[] vertices = {
+        //    -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+        //     0.5f, -0.5f, 0.0f, //Bottom-right vertex
+        //     0.0f,  0.5f, 0.0f  //Top vertex
+        //};
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+           : base(gameWindowSettings, nativeWindowSettings) { }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (!IsFocused) // Check to see if the window is focused
+            {
+                return;
+            }
+
+            var input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Time += 35.0 * e.Time * Side * Math.Cos(Time / 30);
+
+            if (Math.Abs(Time) > Degrees) Side *= -1;
+
+            var RotationMatrixZ = Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(Time));
+            var RotationMatrixY = Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(90));
+            var TranslationMatrix = Matrix4.CreateTranslation(0, 0, (float)(Time / 80));
+
+            var model = Matrix4.Identity * RotationMatrixZ * TranslationMatrix * RotationMatrixY;
+
+            foreach (var Obj in ObjectRenderList)
+            {
+                Obj.Bind();
+                Obj.ApplyTexture();
+                Obj.UpdateShaderModel(model);
+                Obj.ShaderAttribute();
+                Obj.Render();
+            }
+
+            SwapBuffers();
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            //запускается каждый раз как меняется окно
+            base.OnResize(e);
+            GL.Viewport(0, 0, Size.X, Size.Y);
+            
+        }
+        private void DefineShader(Shader Shader)
+        {
+            Shader.SetInt("material.diffuse", 0);
+            Shader.SetInt("material.specular", 1);
+            Shader.SetVector3("material.specular", new Vector3(0.5f, 0.5f, 0.5f));
+            Shader.SetFloat("material.shininess", 100000.0f);
+            Shader.SetVector3("light.position", LightPos);
+            Shader.SetFloat("light.constant", 0.1f);
+            Shader.SetFloat("light.linear", 0.09f);
+            Shader.SetFloat("light.quadratic", 0.032f);
+            Shader.SetVector3("light.ambient", new Vector3(0.2f));
+            Shader.SetVector3("light.diffuse", new Vector3(0.5f));
+            Shader.SetVector3("light.specular", new Vector3(1.0f));
+            Shader.Use();
+        }
+        protected override void OnLoad()
+        {
+
+           base.OnLoad();
+           GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);//цвет окна
+           GL.Enable(EnableCap.DepthTest);
+            ////VertexBufferObject = GL.GenBuffer();//генерация индетиф буфера и запись в переменную для хранения дескриптора
+
+            //Sphere Butt = new Sphere(0.2f, 0.0f, 0.5f, 0f);
+            Sphere Boll1 = new Sphere(0.05f, 0.0f, 0.0f, 0f);
+            Sphere Boll2 = new Sphere(0.05f, 0.0f, -0.5f, 0f);
+            Sphere Boll3 = new Sphere(0.05f, -0.5f, 0.0f, 0f);
+
+            shader = new Shader(@"C:/Users/nnatu/source/repos/CG_OpenGL/shader/shader.vert", @"C:/Users/nnatu/source/repos/CG_OpenGL/shader/lighting.frag");
+            DefineShader(shader);
+
+            DiffuseBoll1 = Texture.LoadFromFile(@"C:/Users/nnatu/source/repos/CG_OpenGL/resourse/red.jpg");
+            SpecularBoll1 = Texture.LoadFromFile(@"C:/Users/nnatu/source/repos/CG_OpenGL/resourse/red_specular.jpg");
+            DiffuseBoll2 = Texture.LoadFromFile(@"C:/Users/nnatu/source/repos/CG_OpenGL/resourse/red.jpg");
+            SpecularBoll2 = Texture.LoadFromFile(@"C:/Users/nnatu/source/repos/CG_OpenGL/resourse/red_specular.jpg");
+            DiffuseBoll3 = Texture.LoadFromFile(@"C:/Users/nnatu/source/repos/CG_OpenGL/resourse/red.jpg");
+            SpecularBoll3 = Texture.LoadFromFile(@"C:/Users/nnatu/source/repos/CG_OpenGL/resourse/red_specular.jpg");
+
+            //DiffuseTail = Texture.LoadFromFile("../../../Resources/head.jpg");
+            //SpecularTail = Texture.LoadFromFile("../../../Resources/head_specular.jpg");
+
+            // var ButtVert = Butt.GetAll(); var ButtInd = Butt.GetIndices();
+            var Vert_1 = Boll1.GetAll(); var Ind_1 = Boll1.GetIndices();
+            var Vert_2 = Boll2.GetAll(); var Ind_2 = Boll2.GetIndices();
+            var Vert_3 = Boll3.GetAll(); var Ind_3 = Boll3.GetIndices();
+
+            //ObjectRenderList.Add(new ObjectRender(ButtVert, ButtInd, Shader, DiffuseTail, SpecularTail));
+          
+            ObjectRenderList.Add(new ObjectRender(Vert_1, Ind_1, shader, DiffuseBoll1, SpecularBoll1));
+            ObjectRenderList.Add(new ObjectRender(Vert_2, Ind_2, shader, DiffuseBoll2, SpecularBoll2));
+            ObjectRenderList.Add(new ObjectRender(Vert_3, Ind_3, shader, DiffuseBoll3, SpecularBoll3));
+        }
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+        }
+
+    }
+}
